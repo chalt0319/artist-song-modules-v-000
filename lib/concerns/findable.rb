8a7809a65a8eff02611f4f8905bc7e87572be980
Courtney Haltiwanger <chalt0319@gmail.com>
2018-07-02 16:59:08 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -3,5 +3,8 @@ module Findable
     def find_by_name(name)
       self.all.detect{|a| a.name == name}
     end
-  end 
+     def to_param
+        name.downcase.gsub(' ', '-')
+      end
+  end
 end 
